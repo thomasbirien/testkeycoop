db1185db7b6972a8e41247ee69f94a65b5decf81
thomasbirien <thomasbirien@gmail.com>
2017-05-22 15:46:46 +0200

    write the create methods but need to corrige the routes

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,4 +1,15 @@
 class AnnouncementsController < ApplicationController
   def create
+    raise
+    @announcement = Announcement.new({
+      title: params[:data][:attributes][:job_offer][:title],
+      description: params[:data][:attributes][:job_offer][:description],
+      customer_name: params[:data][:attributes][:job_offer][:customer][:name]
+      })
+    if @announcement.save
+      head :created
+    else
+      head :no_content
+    end
   end
 end
