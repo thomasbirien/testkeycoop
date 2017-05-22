7bd54620bca8cad348a30514b6624f352930e884
thomasbirien <thomasbirien@gmail.com>
2017-05-22 16:07:14 +0200

    announcement mailer method new_announcement is wrote

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -1,13 +1,8 @@
 class AnnouncementMailer < ApplicationMailer
+  def new_announcement(announcement_id)
+    @announcement = Announcement.find(announcement_id)
+    @url = 'localhost:3000/announcements'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.announcement_mailer.new_announcement.subject
-  #
-  def new_announcement
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    mail to: "facturation@example.com", subject: 'new announcement added'
   end
 end
