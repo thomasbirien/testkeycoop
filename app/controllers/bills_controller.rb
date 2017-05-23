b094bfbce160b67abd6564554a1a3b1c946b1cdc
thomasbirien <thomasbirien@gmail.com>
2017-05-23 15:47:12 +0200

    i confused with my preliminary work for create

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,21 +1,34 @@
 class BillsController < ApplicationController
+  def show
+    @bill = Bill.find(params[:id])
+    respond_to do |format|
+      format.pdf do
+        render pdf: "facture",
+               layout: "pdf.html.erb",
+               page_size: "A4",
+               zoom: 5
+      end
+      format.html
+    end
+  end
+
   def create
-    publication = Publication.find(params[:publication_id])
+    announcement = Announcement.find(params[:announcement_id])
     customer = ""
-    customer << publication.customer_name
+    customer << announcement.customer_name
 
     ref = Bill.reference(customer)
     @bill = Bill.new({
-      title: publication.title,
-      customer_name: publication.customer_name,
-      date_of_publication: publication.created_at,
-      price: 1234,
-      publication_id: params[:publication_id],
+      title: announcement.title,
+      customer_name: announcement.customer_name,
+      date_of_publication: announcement.created_at,
+      price: 1234.56,
+      announcement_id: params[:announcement_id],
       reference_bill: ref
       })
     if @bill.save!
       # redirect_to edit_publication_path(publication)
-      redirect_to edit_publication_path(publication.id, @bill)
+      redirect_to edit_announcement_path(announcement.id, @bill)
     end
   end
 end
