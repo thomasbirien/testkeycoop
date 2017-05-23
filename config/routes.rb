a2bd4c38d470c9dd815abe7e81e4094d3e08b919
thomasbirien <thomasbirien@gmail.com>
2017-05-23 15:23:16 +0200

    forget end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :announcements, only:[:index, :create, :edit, :update] do
     resources :bills, only:[:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
